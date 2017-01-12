ddfd313e63dc8cd2ae554961dfdc3a7074680bdc
Kuba <me@yahoo.com>
2017-01-12 13:45:00 +0100

    delete files

--- footer.php
+++
@@ -1,7 +0,0 @@
-
-
-<div id="footer">
- <p> Czasy Angielskie przyk³ady i informacje, 2011  </p>
-</div>
-
-
--- header.php
+++
@@ -1,5 +0,0 @@
-<div id="header">
-<h1>Czasy Angielskie - tworzenie, æwiczenia, zestawienie</h1>
-<img id="q" src="img.png"
- alt="kr&oacute;lowa uczy angielskiego">
-</div>
